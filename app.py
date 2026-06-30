07c63f6b1b37e56fc80f54c0cb93e45ab80af080
Rama143krishna <ramakrishna_b@srm.edu.in>
2026-06-30 17:02:35 +0530

    Update app for Render

--- app.py
+++ app.py
@@ -82,5 +82,11 @@ def predict_cardio():
         })
 
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000)),
+        debug=False
+    )
